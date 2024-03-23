2e4d40d9fc4c590878734b2c2f6c0b4ff7bb0f56
hanni <102026726+hysong4u@users.noreply.github.com>
2024-03-23 23:39:01 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 732 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool compare(int a, int b) {
+    return a > b;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int *number = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> number[i];
+    }
+
+    sort(number, number + n, compare);
+
+    for (int i = 0; i < n; i++) {
+        cout << number[i]<<"\n" ;
+    }
+
+    delete[] number;
+
+    return 0;
+}
